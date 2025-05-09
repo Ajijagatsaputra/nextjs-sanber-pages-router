071c83d6cd93bf3b199d6f46474ea94629d5e048
Ajijagatsaputra <ajijagat90@gmail.com>
2025-05-09 08:30:12 +0700

    optimizing

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,3 +1,19 @@
+// import Image from 'next/image'
+import ImgHigh from '@/assets/logo-fashion.png'
+
 export default function About() {
-  return <div>About Page</div>
+  return (
+    <div>
+      <h1>About Page</h1>
+      {/* <Image
+        src="/logo-fashion.png"
+        alt="logo"
+        width={1000}
+        height={800}
+        className="object-cover"
+        priority
+      /> */}
+      <img {...ImgHigh} />
+    </div>
+  )
 }
